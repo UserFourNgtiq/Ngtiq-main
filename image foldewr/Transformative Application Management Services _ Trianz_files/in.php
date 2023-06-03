5b0747b66652c02e1bca0e6fe564f7bdddc5f556
user-four-Avinash-roy <130050912+UserFourNgtiq@users.noreply.github.com>
2023-06-03 17:34:19 +0530

    pushing all new file

---
+++ image foldewr/Transformative Application Management Services _ Trianz_files/in.php
@@ -0,0 +1,7 @@
+// static34 (☞ﾟ∀ﾟ)☞
+if( window._cgen ) {
+if( !_cgen_custom.cookies_disable ) _cgen_custom.cookies_disable = 1;
+_cgen.advanced();
+_cgen.ping_start();
+_heatmaps_g2g_101102577='no';
+}
